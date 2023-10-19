9990627f87cd13c780fad1e6eb2aee8a019bf421
Wills Jarquin <jarquinwills@gmail.com>
2023-10-19 19:50:04 -0400

    Initial Commit - Wills Jarquin

---
+++ src/JUnitTests/BusinessObjectTests.java
@@ -0,0 +1,14 @@
+package JUnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BusinessObjectTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
