6f70416ca9f82589b3d00c2f1f747f222cf8e37a
Wills Jarquin <jarquinwills@gmail.com>
2023-10-20 21:44:59 -0400

    Tests completed

--- src/project/WorkFlowObject.java
+++ src/project/WorkFlowObject.java
@@ -1,27 +1,28 @@
 package project;
 
 /**
- * 
+ * This class creates the object that handles the workflow of the system.
  * @author Wills Jarquin
  *
  */
 
 public class WorkFlowObject {
 	
-	/** */
+	/** This variable keeps a series of steps to compare to other object statuses. */
 	private enum step {APPROVE, REVIEW}
-	/** */
+	/** This variable keeps the id of the workflow. */
 	private String id;
 	
 	/**
-	 * 
+	 * This object generates an instance of a WorkFlowObject.
 	 */
 	public WorkFlowObject() {
 		
 	}
 	
 	/**
-	 * 
+	 * This object generates an instance of a WorkFlowObject.
+	 * @param id is the id to assign to the WorkFlowObject.
 	 */
 	public WorkFlowObject(String id) {
 		
@@ -29,8 +30,8 @@ public class WorkFlowObject {
 	
 	
 	/**
-	 * 
-	 * @return
+	 * This function sets the id of the workflow object.
+	 * @param id is the new id to assign the object.
 	 */
 	public void setId(String id) {
 		
@@ -38,8 +39,8 @@ public class WorkFlowObject {
 	
 	
 	/**
-	 * 
-	 * @return
+	 * This method gets the current id of the workflow object
+	 * @return the id of the workflow object.
 	 */
 	public String getId() {
 		return "";
