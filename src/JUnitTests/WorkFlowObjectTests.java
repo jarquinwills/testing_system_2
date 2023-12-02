6800249be28a3265a413269d16422d2599942874
Wills Jarquin <jarquinwills@gmail.com>
2023-12-01 19:38:43 -0500

    first commit

--- src/JUnitTests/WorkFlowObjectTests.java
+++ src/JUnitTests/WorkFlowObjectTests.java
@@ -1,4 +1,5 @@
 package JUnitTests;
+
 import project.WorkFlowObject;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,68 +7,53 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 class WorkFlowObjectTests {
-	
+
 	/**
 	 * 
 	 */
 	@Test
 	void testConstructor() {
-		//generate new Workflow Object.
+		// generate new Workflow Object.
 		WorkFlowObject wfObj = new WorkFlowObject();
-		
-		//Check if WorkflowObject was generated properly.
+
+		// Check if WorkflowObject was generated properly.
 		assertEquals(wfObj.getClass(), WorkFlowObject.class);
 	}
-	
-	/**
-	 * 
-	 */
-	@Test
-	void testConstructorWithParameter() {
-		//Default ID.
-		String id = "1234";
-		
-		//generate new Workflow Object.
-		WorkFlowObject wfObj = new WorkFlowObject(id);
-		
-		//Check if the id was assigned to the predetermined.
-		assertEquals(id, wfObj.getId());
-	}
-	
+
 	/**
 	 * 
 	 */
 	@Test
 	void testSetId() {
-		//Default ID.
+		// Default ID.
 		String id = "1234";
 		String idVariation = "1235";
-		
-		//generate new Workflow Object.
+
+		// generate new Workflow Object.
 		WorkFlowObject wfObj = new WorkFlowObject(id);
-		
-		//Reset ID to variation.
+
+		// Reset ID to variation.
 		wfObj.setId(idVariation);
-		
-		//Check if the id was assigned to the variation.
+
+		// Check if the id was assigned to the variation.
 		assertEquals(idVariation, wfObj.getId());
 	}
-	
+
 	/**
 	 * 
 	 */
 	@Test
 	void testGetId() {
-		//Default ID.
+		// Default ID.
 		String id = "1234";
-		
-		//generate new Workflow Object.
+
+		// generate new Workflow Object.
 		WorkFlowObject wfObj = new WorkFlowObject();
-		
-		//Reset ID to variation.
+
+		// Reset ID to variation.
 		wfObj.setId(id);
-		
-		//Check if the get method matches what was assigned.
+
+		// Check if the get method matches what was assigned.
 		assertEquals(id, wfObj.getId());
 	}
 
